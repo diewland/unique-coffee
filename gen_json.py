bee631c0e22531a438d1f8fc7108b619b017f315
diewland <diewland@gmail.com>
2022-10-11 23:35:03 +0700

    update unrevealed edition

--- gen_json.py
+++ gen_json.py
@@ -3,7 +3,7 @@ from pprint import pprint as pp
  
 NAME = "Unique Coffee"
 DESC = "NFT Unique Coffee of your choice by selecting Volume 2 decimal digits from Apetmism. 3-6 Dollars will be randomly distributed every 2 weeks. Invite everyone to win prizes."
-IMG = "https://diewland.github.io/unique-coffee/png/c{:02}.png"
+IMG = "https://diewland.github.io/unique-coffee/png/cxx.png"
 ENGINE = "Jigsaw Engine"
 
 OUTPUT_DIR = "./json"
@@ -27,9 +27,9 @@ metadata = {
 for id in range(0, MAX_SUPPLY):
 
     # update data
-    metadata["name"] = "{} #{:02}".format(NAME, id)
+    metadata["name"] = "{} #??".format(NAME, id)
     metadata["image"] = IMG.format(id)
-    metadata["attributes"][0]["value"] = ".{:02}".format(id)
+    metadata["attributes"][0]["value"] = "???".format(id)
 
     # debug
     #print(metadata)
